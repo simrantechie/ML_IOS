334b98b0559673a8722f0845fec9906c81d76ed8
Simranjeet <simranjeet@unyscape.com>
2021-11-29 15:20:33 +0530

    Initial Commit

---
+++ ML_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ML_IOS
+//
+//  Created by Desktop-simranjeet on 29/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
